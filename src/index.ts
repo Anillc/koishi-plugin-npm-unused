41994105468aabe4818a50b7274db423fdc2d1fc
Anillc <i@anillc.cn>
2023-04-25 00:36:04 +0800

    implement command

--- src/index.ts
+++ src/index.ts
@@ -4,13 +4,34 @@ import { Context, Schema } from 'koishi'
 import { Stream } from 'stream'
 import { extract } from 'tar'
 import diff from 'lodash.difference'
+import sample from 'lodash.sample'
 import { StringWritable } from './stream'
+import {} from '@koishijs/translator'
 
 export const Config: Schema<{}> = Schema.object({})
 
 export const name = 'npm-unused'
 
-export async function apply(ctx: Context) {
+export function apply(ctx: Context) {
+  let unused: string[]
+  getUnused(ctx).then(result => unused = result)
+  ctx.command('npmunused', '随机一个 npm 上尚未被占用的英文单词包名')
+    .option('raw', '-r 仅输出单词')
+    .action(async ({ options }) => {
+      if (!unused) {
+        return '插件加载中，请稍后重试。'
+      }
+      const word = sample(unused)
+      if (options.raw) return word
+      if (ctx.translator) {
+        const translated = await ctx.translator.translate({ input: word })
+        return `随机到未使用包名: ${word}\n翻译: ${translated}`
+      }
+      return `随机到未使用包名: ${word}`
+    })
+}
+
+async function getUnused(ctx: Context): Promise<string[]> {
   const http = ctx.http.extend({
     endpoint: 'https://registry.npmmirror.com',
   })
@@ -25,6 +46,5 @@ export async function apply(ctx: Context) {
     }
   }))
   const names: string[] = JSON.parse(await writable.promise)
-  const unused = diff(words, names)
-  
+  return diff(words, names)
 }
